e0e83cf3e5ad0028cf0778465e4284ee628131b6
Swetha Sethumadhavan <swetha.sethumadhavan@gmail.com>
2019-06-19 19:09:23 -0600

    fix template file

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -1,12 +1,20 @@
 @extends('master')
 
 @section('todologic')
-
-    Todo List
+ 
+ {{-- <h5 class="card-title">Todo List</h5> --}}
 
      @foreach ($tasks as $task)
 
-       <li>  {{$task->name}}    </li>
+       <li>  {{$task->name}}    
+
+       <button type="button" class="close" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+       </button>
+
+
+       </li>
+
 
     @endforeach
      
